799f0463c8a2fa28475df76d9485d50d50ee652f
Doan Anh  Tien <d.atien228@gmail.com>
2022-07-22 18:30:13 +0700

    remove other parameters

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ longTokenizer = BartTokenizer.from_pretrained('datien228/distilbart-cnn-12-6-ftn
 longModel = BartForConditionalGeneration.from_pretrained('datien228/distilbart-cnn-12-6-ftn-multi_news')
 
 
-def summarize(text, num_beams=5, length_penalty=2.0, max_length=50, min_length=15, no_repeat_ngram_size=3):
+def summarize(text):
     
     text = text.replace('\n','')
 
     text_input_ids = longTokenizer.encode(text, return_tensors='pt', max_length=1024, truncation=True)
-    summary_ids = longModel.generate(text_input_ids, num_beams=int(num_beams),
-                                    length_penalty=float(length_penalty), 
+    summary_ids = longModel.generate(text_input_ids, num_beams=5,
+                                    length_penalty=2.0, 
                                     # max_length=int(max_length)+45,
                                     # min_length=int(min_length)+45, 
-                                    no_repeat_ngram_size=int(no_repeat_ngram_size),
+                                    no_repeat_ngram_size=3,
                                     top_k=50)
 
     long_summary_txt = longTokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True,
